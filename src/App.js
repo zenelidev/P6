95d26cebfcecba876f40c64e210d17ab84ba5c39
Sarah Zeneli <sarahlea1998@gmail.com>
2023-08-31 19:16:45 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Routes>
+        <Route path="./header" exact component={Header} />
+    </Routes>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <div>
+        <h1>This is the header</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
